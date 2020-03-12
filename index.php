30450a053a8c5f93685b337c6c729ece5536fc6e
Matthew Walller <d00218004@student.dkit.ie>
2020-03-12 02:06:02 +0000

    Added a Feature to login into the website, access protected pages and logout of the web application. The following files are added; *config.php *index.php *login.php *logout.php *register.php *welcome.php

--- index.php
+++ index.php
@@ -63,14 +63,7 @@ $statement3->closeCursor();
         <a class="nav-link" href="./wearables.php">Wearables</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+      <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
       </li>
     </ul>
   </div>
